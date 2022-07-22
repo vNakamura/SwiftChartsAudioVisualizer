30d5fc999a8f32ac3925f7bb01ffb2395147983e
Vinicius Nakamura <hello@vnakamura.com>
2022-07-22 17:33:38 -0300

    Create basic chart to start off

--- SwiftChartsAudioVisualizer/ContentView.swift
+++ SwiftChartsAudioVisualizer/ContentView.swift
@@ -5,16 +5,21 @@
 //  Created by Vinicius Nakamura on 22/07/22.
 //
 
+import Charts
 import SwiftUI
 
 struct ContentView: View {
+    @State var data: [Float] = Array(repeating: 0, count: 15)
+        .map { _ in Float.random(in: 1 ... 20) }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Chart(Array(data.enumerated()), id: \.0) { index, magnitude in
+            BarMark(
+                x: .value("Frequency", String(index)),
+                y: .value("Magnitude", magnitude)
+            )
         }
+        .padding()
     }
 }
 
