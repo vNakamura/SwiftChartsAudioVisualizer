8849492e3c894b0cecebcbf1e45193f4fa88106b
Vinicius Nakamura <hello@vnakamura.com>
2022-07-23 09:36:16 -0300

    Initial Commit

---
+++ SwiftChartsAudioVisualizer/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftChartsAudioVisualizer
+//
+//  Created by Vinicius Nakamura on 22/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftChartsAudioVisualizer/SwiftChartsAudioVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsAudioVisualizerApp.swift
+//  SwiftChartsAudioVisualizer
+//
+//  Created by Vinicius Nakamura on 22/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsAudioVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
